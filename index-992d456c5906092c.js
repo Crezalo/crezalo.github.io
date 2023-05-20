0a6c008a782691482f1b127781d1e9b0d8256bbd
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2023-05-20 16:31:39 +0530

    minor google analytics changes

--- index-992d456c5906092c.js
+++ index-992d456c5906092c.js
@@ -51,53 +51,53 @@
                             }), (0, r.jsxs)("div", {
                                 className: "flex items-center gap-x-8 mt-6 h-8",
                                 children: [(0, r.jsx)("a", {
-                                    "aria-label": "instagram",
-                                    href: t.instagram,
-                                    target: "_blank",
-                                    rel: "noreferrer",
-                                    children: (0, r.jsx)("svg", {
-                                        className: "fill-current text-gray-800 dark:text-white hover:text-primary",
-                                        xmlns: "http://www.w3.org/2000/svg",
-                                        width: "25px",
-                                        height: "25px",
-                                        viewBox: "0 0 50 50",
-                                        children: (0, r.jsx)("path", {
-                                            d: "M 16 3 C 8.83 3 3 8.83 3 16 L 3 34 C 3 41.17 8.83 47 16 47 L 34 47 C 41.17 47 47 41.17 47 34 L 47 16 C 47 8.83 41.17 3 34 3 L 16 3 z M 37 11 C 38.1 11 39 11.9 39 13 C 39 14.1 38.1 15 37 15 C 35.9 15 35 14.1 35 13 C 35 11.9 35.9 11 37 11 z M 25 14 C 31.07 14 36 18.93 36 25 C 36 31.07 31.07 36 25 36 C 18.93 36 14 31.07 14 25 C 14 18.93 18.93 14 25 14 z M 25 16 C 20.04 16 16 20.04 16 25 C 16 29.96 20.04 34 25 34 C 29.96 34 34 29.96 34 25 C 34 20.04 29.96 16 25 16 z"
+                                        "aria-label": "instagram",
+                                        href: t.instagram,
+                                        target: "_blank",
+                                        rel: "noreferrer",
+                                        children: (0, r.jsx)("svg", {
+                                            className: "fill-current text-gray-800 dark:text-white hover:text-primary",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            width: "25px",
+                                            height: "25px",
+                                            viewBox: "0 0 50 50",
+                                            children: (0, r.jsx)("path", {
+                                                d: "M 16 3 C 8.83 3 3 8.83 3 16 L 3 34 C 3 41.17 8.83 47 16 47 L 34 47 C 41.17 47 47 41.17 47 34 L 47 16 C 47 8.83 41.17 3 34 3 L 16 3 z M 37 11 C 38.1 11 39 11.9 39 13 C 39 14.1 38.1 15 37 15 C 35.9 15 35 14.1 35 13 C 35 11.9 35.9 11 37 11 z M 25 14 C 31.07 14 36 18.93 36 25 C 36 31.07 31.07 36 25 36 C 18.93 36 14 31.07 14 25 C 14 18.93 18.93 14 25 14 z M 25 16 C 20.04 16 16 20.04 16 25 C 16 29.96 20.04 34 25 34 C 29.96 34 34 29.96 34 25 C 34 20.04 29.96 16 25 16 z"
+                                            })
                                         })
-                                    })
-                                }), (0, r.jsx)("a", {
-                                    "aria-label": "linkedin",
-                                    href: t.linkedin,
-                                    target: "_blank",
-                                    rel: "noreferrer",
-                                    children: (0, r.jsx)("svg", {
-                                        className: "fill-current text-gray-800 dark:text-white hover:text-primary",
-                                        xmlns: "http://www.w3.org/2000/svg",
-                                        width: "24",
-                                        height: "24",
-                                        viewBox: "0 0 24 24",
-                                        children: (0, r.jsx)("path", {
-                                            d: "M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                                    }), (0, r.jsx)("a", {
+                                        "aria-label": "linkedin",
+                                        href: t.linkedin,
+                                        target: "_blank",
+                                        rel: "noreferrer",
+                                        children: (0, r.jsx)("svg", {
+                                            className: "fill-current text-gray-800 dark:text-white hover:text-primary",
+                                            xmlns: "http://www.w3.org/2000/svg",
+                                            width: "24",
+                                            height: "24",
+                                            viewBox: "0 0 24 24",
+                                            children: (0, r.jsx)("path", {
+                                                d: "M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                                            })
                                         })
-                                    })
-                                }), 
-                                // (0, r.jsx)("a", {
-                                //     "aria-label": "twitter",
-                                //     href: t.twitter,
-                                //     target: "_blank",
-                                //     rel: "noreferrer",
-                                //     children: (0, r.jsx)("svg", {
-                                //         className: "fill-current text-gray-800 dark:text-white hover:text-primary",
-                                //         xmlns: "http://www.w3.org/2000/svg",
-                                //         width: "24",
-                                //         height: "24",
-                                //         viewBox: "0 0 24 24",
-                                //         children: (0, r.jsx)("path", {
-                                //             d: "M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z"
-                                //         })
-                                //     })
-                                // })
-                            ]
+                                    }),
+                                    // (0, r.jsx)("a", {
+                                    //     "aria-label": "twitter",
+                                    //     href: t.twitter,
+                                    //     target: "_blank",
+                                    //     rel: "noreferrer",
+                                    //     children: (0, r.jsx)("svg", {
+                                    //         className: "fill-current text-gray-800 dark:text-white hover:text-primary",
+                                    //         xmlns: "http://www.w3.org/2000/svg",
+                                    //         width: "24",
+                                    //         height: "24",
+                                    //         viewBox: "0 0 24 24",
+                                    //         children: (0, r.jsx)("path", {
+                                    //             d: "M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z"
+                                    //         })
+                                    //     })
+                                    // })
+                                ]
                             }), (0, r.jsx)("div", {
                                 className: "flex items-center mt-6",
                                 children: (0, r.jsxs)("p", {
@@ -138,7 +138,7 @@
                     }), [e.events]), (0, r.jsxs)(r.Fragment, {
                         children: [(0, r.jsx)(c.default, {
                             strategy: "afterInteractive",
-                            src: "https://www.googletagmanager.com/gtag/js?id=".concat(m)
+                            src: "https://www.googletagmanager.com/gtag/js?id=".concat("G-XVYBBXLTFH")
                         }), (0, r.jsx)(c.default, {
                             id: "gtag-init",
                             strategy: "afterInteractive",
